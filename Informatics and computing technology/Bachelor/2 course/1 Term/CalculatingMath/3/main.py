226ef2069085ec39536ad33e3bd03e9e8a7234ad
Sashavav <vav110011@gmail.com>
2024-10-06 19:07:45 +0300

    hotfix 3rd practice CalcMath

--- Informatics and computing technology/Bachelor/2 course/1 Term/CalculatingMath/3/main.py
+++ Informatics and computing technology/Bachelor/2 course/1 Term/CalculatingMath/3/main.py
@@ -6,34 +6,44 @@ def f(function_x):
     return a * function_x ** 3 + b * a * function_x ** 2 - a * function_x - b * a
 
 
+def error_f(function_x, epsilon=0.001):
+    return f(function_x - epsilon) * f(function_x + epsilon)
+
+
 def df(function_x):
     return a * 3 * function_x ** 2 + 2 * a * b * function_x - a
 
 
 def simple_iteration():
-    alpha = -0.05
-    x = -5.74
-    for i in range(6):
-        print(i + 1, x, round(f(x) * 100000) / 100000)
-        x += alpha * round(f(x) * 100000) / 100000
+    alpha = 0.06
+    x = -3
+    i = 0
+    while error_f(x) > 0:
+        print(i, x, f(x), error_f(x))
+        x -= alpha * f(x)
+        i += 1
+    print(i, x, f(x), error_f(x))
     return
 
 
 def chord():
     x1 = -b + 0.5
     x2 = -0.5
-    for i in range(7):
-        print(i + 1, x1, f(x1))
+    i = 0
+    while error_f(x1) > 0:
+        print(i, x1, f(x1), error_f(x1))
         k = (f(x2) - f(x1)) / (x2 - x1)
         x1 = x1 - f(x1) / k
+        i += 1
+    print(i, x1, f(x1), error_f(x1))
     return
 
 
 def tangent():
     x = 2
     for i in range(7):
-        print(i+1, x, f(x))
-        x -= f(x)/df(x)
+        print(i, x, df(x), f(x),error_f(x))
+        x -= f(x) / df(x)
 
 
 tangent()

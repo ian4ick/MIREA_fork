d92cc7ebec01740773545a3ec66be9c5a16612a5
Sashavav <vav110011@gmail.com>
2024-10-27 16:59:28 +0300

    Fix practice8 problem when program is infinite cause all the threads are waiting

--- InfAndCompTechnology/Bachelor/2 course/1 Term/Java/practice8/src/Main.java
+++ InfAndCompTechnology/Bachelor/2 course/1 Term/Java/practice8/src/Main.java
@@ -1,38 +1,39 @@
 import java.util.Date;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     private static final Object lock = new Object();
+    private static final int FINISH_DISTANCE = 300;
     public static void main(String[] args) {
-        AtomicInteger maxNumber = new AtomicInteger();
-        maxNumber.set(20);
         AtomicInteger currentNumber = new AtomicInteger();
-        AtomicBoolean timeBoolean = new AtomicBoolean();
         AtomicLong currentTimeBoolean = new AtomicLong();
         currentTimeBoolean.set(System.currentTimeMillis());
-        AtomicLong startTimeBoolean = new AtomicLong();
         AtomicInteger stopRace = new AtomicInteger();
         stopRace.set(0);
-        startTimeBoolean.set(System.currentTimeMillis());
-        timeBoolean.set(false);
+        currentNumber.set(2);
         Thread firstThread = new Thread(
                 () -> {
                     Racer racer1 = new Racer(30, 10, 30, 2, 0);
-                    while (racer1.getDistance() < 300) {
+                    while (racer1.getDistance() < FINISH_DISTANCE) {
                         synchronized (lock) {
-                            if (currentNumber.get() % 2 == 0  && timeBoolean.get()) {
+                            if (currentNumber.get() == 0) {
                                 racer1.setDistance(new Random().nextInt(racer1.getDistance(), racer1.getDistance() + 30));
                                 System.out.print("Racer 1: " + racer1.getDistance() + " ");
-                                currentNumber.incrementAndGet();
-                                if (stopRace.get() > 0)
-                                    timeBoolean.set(false);
+                                if (stopRace.get() == 0)
+                                    currentNumber.set(1);
+                                else{
+                                    currentNumber.set(2);
+                                    System.out.println();
+                                }
+                                if (racer1.getDistance() >= FINISH_DISTANCE)
+                                    stopRace.incrementAndGet();
                                 lock.notify();
                             }
                             else{
                                 try{
+                                    lock.notify();
                                     lock.wait();
                                 }
                                 catch (InterruptedException e){
@@ -42,23 +43,24 @@ public class Main {
                         }
                     }
                     System.out.println("\nRacer 1: " + "Finished" + " ");
-                    stopRace.incrementAndGet();
                 }
         );
         Thread secondThread = new Thread(
                 () -> {
                     Racer racer2 = new Racer(20, 10, 60, 3, 0);
-                    while (racer2.getDistance() < 300) {
+                    while (racer2.getDistance() < FINISH_DISTANCE) {
                         synchronized (lock) {
-                            if (currentNumber.get() % 2 == 1  && timeBoolean.get()) {
+                            if (currentNumber.get() == 1 || (currentNumber.get() % 3 == 0 && stopRace.get() == 1)) {
                                 racer2.setDistance(new Random().nextInt(racer2.getDistance(), racer2.getDistance() + 30));
                                 System.out.println("Racer 2: " + racer2.getDistance() + " ");
-                                currentNumber.incrementAndGet();
-                                timeBoolean.set(false);
+                                currentNumber.set(2);
+                                if (racer2.getDistance() >= FINISH_DISTANCE)
+                                    stopRace.incrementAndGet();
                                 lock.notify();
                             }
                             else{
                                 try{
+                                    lock.notify();
                                     lock.wait();
                                 }
                                 catch (InterruptedException e){
@@ -68,27 +70,30 @@ public class Main {
                         }
                     }
                     System.out.println("\nRacer 2: " + "Finished" + " ");
-                    stopRace.incrementAndGet();
                 }
         );
         Thread timeThread = new Thread(
                 () -> {
                     while (stopRace.get() < 2) {
                         synchronized (lock) {
-                            if (!timeBoolean.get() && (System.currentTimeMillis() - currentTimeBoolean.get()) > 999) {
-                                timeBoolean.set(true);
+                            if ((System.currentTimeMillis() - currentTimeBoolean.get()) > 999 && currentNumber.get() == 2) {
                                 currentTimeBoolean.set(System.currentTimeMillis());
                                 System.out.print("Current time: " + new Date(currentTimeBoolean.get()) + ": ");
+                                currentNumber.set(0);
                                 lock.notify();
-                            } else {
+                            }
+                            else {
                                 try {
-                                    lock.notifyAll();
-                                    if ((System.currentTimeMillis() - currentTimeBoolean.get()) < 999)
-                                        lock.wait(1000 - System.currentTimeMillis() + currentTimeBoolean.get());
-                                    else
+                                    if (stopRace.get() < 2){
+                                        lock.notify();
                                         lock.wait();
+                                    }
+                                    else{
+                                        System.out.println("\nThank you for watching this race");
+                                        System.exit(0);
+                                    }
                                 } catch (InterruptedException e) {
-                                    Thread.currentThread().interrupt();
+                                    throw new RuntimeException(e);
                                 }
                             }
 

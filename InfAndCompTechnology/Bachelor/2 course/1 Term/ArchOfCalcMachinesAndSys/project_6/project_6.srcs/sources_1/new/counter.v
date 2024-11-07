5cab8c87b3755c30b39355269360c044f86a1529
Sashavav <vav110011@gmail.com>
2024-11-07 19:01:36 +0300

    Update ACM&S practice 6 add counter direction

--- InfAndCompTechnology/Bachelor/2 course/1 Term/ArchOfCalcMachinesAndSys/project_6/project_6.srcs/sources_1/new/counter.v
+++ InfAndCompTechnology/Bachelor/2 course/1 Term/ArchOfCalcMachinesAndSys/project_6/project_6.srcs/sources_1/new/counter.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module counter#(step = 1, mod = 151)(
+module counter#(step = 1, mod = 151, mode = 1)(
 input clk,
 output [7:0] cnt
     );
@@ -9,6 +9,11 @@ begin
     a = 0;
 end
 always @(clk)
+begin
+if (mode==1)
     a <= (a + step) % mod;
+else
+    a <= (a - step) % mod;
+end
 assign cnt = a;
 endmodule

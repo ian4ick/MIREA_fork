8d30f8c5a6182e24d31d0432cb808955cdd1b173
Sashavav <vav110011@gmail.com>
2024-11-17 20:58:04 +0300

    Add Calc Math practice 5

--- InfAndCompTechnology/Bachelor/2 course/1 Term/CalculatingMath/5.py
+++ InfAndCompTechnology/Bachelor/2 course/1 Term/CalculatingMath/5.py
@@ -1,6 +1,15 @@
 import numpy as np
 
-
+A1 = np.array([
+    [5., 5., 5.],
+    [7., 7., 3.],
+    [4., 1., 3.]
+])
+B1 = np.array([
+    0., -4., -1.
+])
+C1 = np.linalg.solve(A1, B1)
+print(C1)
 A1 = np.array([
     [5., 5., 5.],
     [4., 1., 3.],
@@ -12,14 +21,16 @@ A2 = np.array([
     [1., 1., 5.]
 ])
 B1 = np.array([
-    0., -4., -1.
+    0., -1., -4.
 ])
 B2 = np.array([
     -1., 4., 6.
 ])
 C1 = np.linalg.solve(A1, B1)
 C2 = np.linalg.solve(A2, B2)
+print("True answers")
 print(C1, C2, sep='\n')
+print("Solution")
 
 
 def gauss(A, B):
@@ -44,19 +55,58 @@ def gauss(A, B):
             answers[i] -= A[i, j] * answers[j]
     print(*answers)
 
-#gauss(A1, B1)
-#gauss(A2, B2)
+
+gauss(A1, B1)
+gauss(A2, B2)
+
+print('\n' + '-' * 100 + '\n')
+
+
+def orthogonal(R, A, B):
+    T = np.zeros((2, 2))
+    T[0, 0] = np.dot(R[0], A[1]) / np.dot(R[0], R[0])
+    print(np.dot(R[0], A[1]), np.dot(R[0], R[0]), T[0, 0])
+    R[1] -= T[0, 0] * R[0]
+    T[0, 1] = np.dot(R[0], A[2]) / np.dot(R[0], R[0])
+    print(np.dot(R[0], A[2]), np.dot(R[0], R[0]), T[0, 1])
+    T[1, 1] = np.dot(R[1], A[2]) / np.dot(R[1], R[1])
+    print(np.dot(R[1], A[2]), np.dot(R[1], R[1]), T[1, 1])
+    R[2] -= T[0, 1] * R[0] + T[1, 1] * R[1]
+    R = R.transpose()
+    print(R)
+    print(T)
+    R = R.transpose()
+    X = np.zeros(3)
+    X[2] = np.dot(R[2], B) / np.dot(R[2], A[2])
+    print(np.dot(R[2], B), np.dot(R[2], A[2]), X[2])
+    B -= X[2] * A[2]
+    print(X[2] * A[2], B)
+    X[1] = np.dot(R[1], B) / np.dot(R[1], A[1])
+    print(np.dot(R[1], B), np.dot(R[1], A[1]), X[1])
+    B -= X[1] * A[1]
+    print(X[1] * A[1], B)
+    X[0] = np.dot(R[0], B) / np.dot(R[0], A[0])
+    print(np.dot(R[0], B), np.dot(R[0], A[0]), X[0])
+    return X
+
 
 A1 = np.array([
-    [5, 5, 5],
-    [7, 7, 3],
-    [4, 1, 3]
+    [5., 5., 5.],
+    [4., 1., 3.],
+    [7., 7., 3.]
 ])
 A2 = np.array([
-    [4, 1, 1],
-    [2, 1, 4],
-    [1, 1, 5]
+    [4., 1., 1.],
+    [2., 1., 4.],
+    [1., 1., 5.]
 ])
-A1 = A1.transpose()
-A2 = A2.transpose()
-print(A1, A2)
+B1 = np.array([
+    0., -1., -4.
+])
+B2 = np.array([
+    -1., 4., 6.
+])
+C1 = orthogonal(A1.transpose().copy(), A1.transpose(), B1)
+print("Answer", C1)
+C2 = orthogonal(A2.transpose().copy(), A2.transpose(), B2)
+print("Answer", C2)

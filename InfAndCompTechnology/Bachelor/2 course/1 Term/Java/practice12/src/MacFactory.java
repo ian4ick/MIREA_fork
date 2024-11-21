6ade44977fa3ff1007f258bbda8ef31c69ebf6b1
Sashavav <vav110011@gmail.com>
2024-11-21 07:16:30 +0300

    Add Java practice 12

---
+++ InfAndCompTechnology/Bachelor/2 course/1 Term/Java/practice12/src/MacFactory.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class MacFactory implements GUIFactory{
+
+    @Override
+    public Button createButton() {
+        return new MacButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacCheckbox();
+    }
+}
---
+++ InfAndCompTechnology/Bachelor/2 course/1 Term/Java/practice12/src/WinFactory.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class WinFactory implements GUIFactory{
+    @Override
+    public Button createButton() {
+        return new WinButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WinCheckbox();
+    }
+}
